8f29286396e06a708fa55f568c76fbdcd056af46
Tarun Parmar <tarunparmar@gmail.com>
2018-08-02 23:51:20 -0500

    push section 12

--- Section 12: Objet Oriented Python/accounts.py
+++ Section 12: Objet Oriented Python/accounts.py
@@ -17,3 +17,12 @@ class Account:
     def show_balance(self):
         print("Balance is {}".format(self.balance))
 
+
+if __name__ == '__main__':
+    tim = Account("Tim", 0)
+    tim.show_balance()
+
+    tim.deposit(1000)
+    tim.show_balance()
+    tim.withdraw(500)
+    tim.show_balance()
